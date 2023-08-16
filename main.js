c3a47aa412728b8ca1e9d096ab770892d5f85172
kingbookk <kingbooktashkent@gmail.com>
2023-08-16 15:14:27 +0500

    m

---
+++ main.js
@@ -0,0 +1,18 @@
+// const menuBtn = document.querySelector('.menu-btn');
+// let menuOpen = false;
+
+// menuBtn.addEventListener('click', () => {
+//     if(!menuOpen) {
+//       menuBtn.classList.add('open');
+//       menuOpen = true;
+//     } else {
+//       menuBtn.classList.remove('open');
+//       menuOpen = false;
+//     }
+//   });
+
+
+
+function moon() {
+    document.body.style.backgroundColor = 'white';
+}
